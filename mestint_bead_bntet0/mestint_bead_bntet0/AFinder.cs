8eabe805b34a831b1840d6565bde5d413371515b
Kristóf Levente Pápai <83229957+KristofPapai@users.noreply.github.com>
2022-06-07 17:06:56 +0200

    Graphfinder

--- mestint_bead_bntet0/mestint_bead_bntet0/AFinder.cs
+++ mestint_bead_bntet0/mestint_bead_bntet0/AFinder.cs
@@ -8,5 +8,15 @@ namespace mestint_bead_bntet0
 {
     public abstract class AFinder
     {
+        public Peak startPeak;
+        public AFinder(Peak startPeak)
+        {
+            this.startPeak = startPeak;
+        }
+
+        public Peak GetStartPeak()
+        {
+            return startPeak;
+        }
     }
 }
